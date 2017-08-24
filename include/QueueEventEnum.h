01784690a9dd1f22e3a0511e4c6effae4d1bbd3b
Friedemann Stoffregen <friedemann.stoffregen@student.fh-kiel.de>
2017-08-24 10:05:54 +0200

    Added QueueEventEnum.h

---
+++ include/QueueEventEnum.h
@@ -0,0 +1,14 @@
+//
+// Created by Friedemann Stoffregen on 24.08.17.
+//
+
+#ifndef SAMPLERV2_QUEUEEVENTENUM_H
+#define SAMPLERV2_QUEUEEVENTENUM_H
+enum class QueueEventEnum {
+    BUTTON_DOWN,
+    BUTTON_UP,
+    ROTARY_LEFT,
+    ROTARY_RIGHT
+};
+
+#endif //SAMPLERV2_QUEUEEVENTENUM_H
